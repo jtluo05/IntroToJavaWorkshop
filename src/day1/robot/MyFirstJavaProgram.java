3f0b65fdd459445e54ff71763ea00da4edee0067
The League of Amazing Programmers <league@WTS9.attlocal.net>
2017-03-12 15:11:17 -0700

    i made the robot move

--- src/day1/robot/MyFirstJavaProgram.java
+++ src/day1/robot/MyFirstJavaProgram.java
@@ -1,15 +1,27 @@
 package day1.robot;
 
 import java.awt.Color;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class MyFirstJavaProgram {
 	public static void main(String[] args) {
-		
+
 		// START HERE
 
-	
-		
-		
+		Robot bob = new Robot();
+		bob.penDown();
+		bob.setPenWidth(1000);
+		bob.setPenColor(Color.green);
+		bob.setSpeed(20);
+		bob.turn(90);
+		bob.move(200);
+		bob.turn(90);
+		bob.move(200);
+		bob.turn(90);
+		bob.move(200);
+		bob.turn(90);
+		bob.move(200);
+
 	}
 }
